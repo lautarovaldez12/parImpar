a05109a28de0f23fded81dee3cdc7009bc62a9ae
lautarovaldez12 <74670271+lautarovaldez12@users.noreply.github.com>
2020-12-18 21:54:17 -0300

    Primer commit

---
+++ parImpar.js
@@ -0,0 +1,11 @@
+let parImpar = function(numero){
+    let cuenta = numero % 2;
+    if(cuenta === 0){
+        return console.log("el numero es par")
+    } else if(cuenta !== 0) {
+        return console.log("el numero es impar")
+    }
+}
+
+parImpar(0)
+
